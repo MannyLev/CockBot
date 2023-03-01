6085f16b7029701a930f683aab7f04690b5e9dea
MannyLev <94080012+MannyLev@users.noreply.github.com>
2023-03-01 18:00:33 -0500

    Added motors to the cockBot

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,18 +8,22 @@ import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Drivetrain extends SubsystemBase {
   private WPI_VictorSPX frontRightMotor = new WPI_VictorSPX(19);
   private WPI_VictorSPX backRightMotor = new WPI_VictorSPX(10);
   private WPI_VictorSPX frontLeftMotor = new WPI_VictorSPX(17);
   private WPI_VictorSPX backLeftMotor = new WPI_VictorSPX(11);
+  private MotorControllerGroup lefMotorControllerGroup;
+  private MotorControllerGroup rightMotorControllerGroup;
   
   public Drivetrain() {
     frontRightMotor.setInverted(false);
     frontLeftMotor.setInverted(false);
     backLeftMotor.setInverted(false);
     backRightMotor.setInverted(false);
-
+    this.lefMotorControllerGroup = new MotorControllerGroup(backLeftMotor, frontLeftMotor);
+    this.rightMotorControllerGroup = new MotorControllerGroup(backRightMotor, frontRightMotor);
   }
 }
